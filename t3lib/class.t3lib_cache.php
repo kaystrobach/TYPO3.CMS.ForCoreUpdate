942782a2f96d2b7dce4119911e893d5b5f02d090
Ingo Renner <ingo.renner@typo3.org>
2008-09-24 16:37:55 +0000

    Added class t3lib_cache to easily initialize the various caches in TYPO3 (cache_pages only for now)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/hackontest_2008@4194 709f56b5-9817-0410-a4d7-c38de5d9e867

---
+++ t3lib/class.t3lib_cache.php
@@ -0,0 +1,56 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2008 Ingo Renner <ingo@typo3.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+/**
+ * A cache handling helper class
+ *
+ * @author	Ingo Renner <ingo@typo3.org>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
+class t3lib_cache {
+
+	public static function initPageCache() {
+		try {
+			$GLOBALS['TYPO3_CACHE']->create(
+				'cache_pages',
+				't3lib_cache_VariableCache',
+				't3lib_cache_backend_Db', // TODO make the backend configurable
+				array(
+					'cacheTable' => 'cache_pages'
+				)
+			);
+		} catch(t3lib_cache_exception_DuplicateIdentifier $e) {
+				// do nothing, a cache_pages cache already exists
+		}
+	}
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_cache.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_cache.php']);
+}
+
+?>
