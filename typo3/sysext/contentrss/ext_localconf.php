f46577385b9acc4ada3b02d9f5fd296693058377
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2008-09-25 09:07:20 +0000

    More clean up
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/hackontest_2008@4245 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/contentrss/ext_localconf.php
+++ typo3/sysext/contentrss/ext_localconf.php
@@ -1,11 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-  ## Extending TypoScript from static template uid=43 to set up userdefined tag:
-t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
-	tt_content.CSS_editor.ch.tx_contentrss_pi1 = < plugin.tx_contentrss_pi1.CSS_editor
-',43);
+t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_contentrss_pi1.php', '_pi1', 'list_type', true);
 
-
-t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_contentrss_pi1.php','_pi1','list_type',1);
 ?>
--- typo3/sysext/contentrss/ext_tables.php
+++ typo3/sysext/contentrss/ext_tables.php
@@ -1,26 +1,27 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-$tempColumns = Array (
-	"tx_contentrss_excluderss" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:contentrss/locallang_db.xml:tt_content.tx_contentrss_excluderss",		
-		"config" => Array (
-			"type" => "check",
+
+$tempColumns = array(
+	'tx_contentrss_excluderss' => Array (
+		'exclude' => 1,
+		'label' => 'LLL:EXT:contentrss/locallang_db.xml:tt_content.tx_contentrss_excluderss',
+		'config' => Array (
+			'type' => 'check',
 		)
 	),
 );
 
 
-t3lib_div::loadTCA("tt_content");
-t3lib_extMgm::addTCAcolumns("tt_content",$tempColumns,1);
+t3lib_div::loadTCA('tt_content');
+t3lib_extMgm::addTCAcolumns('tt_content', $tempColumns, true);
 
 
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key';
 
 
-t3lib_extMgm::addPlugin(array('LLL:EXT:contentrss/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
+t3lib_extMgm::addPlugin(array('LLL:EXT:contentrss/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'),'list_type');
 
 
-t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","RSS from Content");
+t3lib_extMgm::addStaticFile($_EXTKEY,'pi1/static/', 'RSS from Content');
 ?>
